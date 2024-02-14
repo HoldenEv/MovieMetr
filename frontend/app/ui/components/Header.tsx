37ed6d5c7ad28a70c15e2d2b9d98a6af621195ff
Lukas Chou <lukaschou21@icloud.com>
2024-02-13 21:33:55 -0800

    NavBar: desktop

--- frontend/app/ui/components/Header.tsx
+++ frontend/app/ui/components/Header.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import styles from "./Header.module.css";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header>
-      <nav>
+      <nav className={styles.navMenu}>
+        <h1>LOGO</h1>
         <ul className={styles.list}>
-          <li>adf</li>
-          <input type="text" placeholder="Search..." />
-          <li>asdf</li>
+          <input
+            type="text"
+            placeholder="Search..."
+            className={styles.search}
+          />
+          <Link href="#" className={styles.navLink}>
+            SIGN IN
+          </Link>
+          <Link href="#" className={styles.navLink}>
+            CREATE ACCOUNT
+          </Link>
         </ul>
+        <div className={styles.hamburger}>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </div>
       </nav>
     </header>
   );
