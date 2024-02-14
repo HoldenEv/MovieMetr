649df4bb479fb2d353ca744ece4cb5f9bbfe7857
Holden <holdenevers@gmail.com>
2024-02-14 15:02:34 -0800

    added simple structure to login

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google"; // TODO: FIND FONTS
 import "@/ui/global.css";
 import Header from "@/ui/components/Header";
+import Login from "@/ui/components/authentication/login/page"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
+        <Login /> 
         {children}
       </body>
     </html>
