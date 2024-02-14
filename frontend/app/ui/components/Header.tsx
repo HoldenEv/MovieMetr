6fceb2f9a548b99e8fec0e98f75c5ed201baad50
Lukas Chou <lukaschou21@icloud.com>
2024-02-13 22:56:02 -0800

    navbar basics done

--- frontend/app/ui/components/Header.tsx
+++ frontend/app/ui/components/Header.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import styles from "./Header.module.css";
 import Link from "next/link";
+// import Image from "next/image";
+// import searchIcon from "/Users/lukaschou/fortnite-meter/MovieMetr/frontend/public/search-svgrepo-com.svg"
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <nav className={styles.navMenu}>
-        <Link href="#" className={styles.logo}>LOGO</Link>
+        <Link href="#" className={styles.logo}>
+          LOGO
+        </Link>
         <ul className={styles.list}>
           <input
             type="text"
             placeholder="Search..."
             className={styles.search}
           />
+          {/* <Image src={searchIcon} alt="Search for a movie"></Image> */}
           <Link href="#" className={styles.navLink}>
             SIGN IN
           </Link>
-          <Link href="#" className={`${styles.navLink} ${styles.createAccount}`}>
+          <Link
+            href="#"
+            className={`${styles.navLink} ${styles.createAccount}`}
+          >
             CREATE ACCOUNT
           </Link>
         </ul>
