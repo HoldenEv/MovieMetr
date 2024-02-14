68c984b04756676ae0a4ff3519eb562cfac1a1f6
Lukas Chou <lukaschou21@icloud.com>
2024-02-13 17:09:16 -0800

    fortnite

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,8 +3,9 @@ import styles from "@/ui/home.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1>TEST</h1>
+    <main className={styles.home}>
+      <h1>Top 10 hottest Fortnite skins</h1>
+      <a href="https://www.fortnite.com/?lang=en-US">fortnite</a>
     </main>
   );
 }
