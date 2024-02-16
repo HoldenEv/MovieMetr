18531d617e15b59b4698133551414d92ca18ed54
Lukas Chou <lukaschou21@icloud.com>
2024-02-16 12:54:49 -0800

    backend file configuration

---
+++ back-end/routes/index.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response, Router } from "express";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript Express!");
+});
+
+export default router;
