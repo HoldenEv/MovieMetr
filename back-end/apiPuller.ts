26f3c09964e649da16162c838d4c98632a040122
Nick Li <nmli@calpoly.edu>
2024-02-20 12:33:53 -0800

    added a bunch of TMDB queries, and api routes to run them

--- back-end/apiPuller.ts
+++ back-end/apiPuller.ts
@@ -4,6 +4,8 @@ const apiAccessToken='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2M2NTAyNGEyZmIzNmI0ZWI1Z
 //key can be used inline of http request, couldnt get it to work though
 const apiKey='b3c65024a2fb36b4eb5f472f5e8ea69d';
 
+
+//search for movies given a search string, currently returns id, title, image, and summary
 const searchMovies = async (searchString: string) => {
     const url = 'https://api.themoviedb.org/3/search/movie?api_key='+apiKey+'&query='+searchString;
     const options = {
@@ -14,6 +16,8 @@ const searchMovies = async (searchString: string) => {
     };
     try{
         const response = await axios.get(url, options);
+        //can edit this function to return more data if needed
+        //calling apiById(with movie.id) will return all data for now
         const movies = response.data.results.map((movie: any) => ({
             id:movie.id,
             title:movie.title,
@@ -26,23 +30,161 @@ const searchMovies = async (searchString: string) => {
         console.error('Error searching movies', error);
         throw error;
     }
-}    
+}   
 
-export {searchMovies};
-//type can be movie, tv, person
-//time can be 'day' or 'week' specifying trending today or this week
-// async function getTrendingMovies(type: string, time: string){
-//     try{
-//         const response = await fetch('https://api.themoviedb.org/3/trending/'+type+'/'+time+'?api_key='+apiKey, options);
-//         const data = await response.json();
-//         return data;
-//     }
-//     catch(error){
-//         console.log(error);
-//     }finally{
-//         console.log('done');
-//     }
-// }
+//searchByActor returns a list of actors and their information
+const searchByActor = async (searchString: string) => {
+    const url = 'https://api.themoviedb.org/3/search/person?api_key='+apiKey+'&query='+searchString;
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+apiAccessToken
+        }
+    };
+    try{
+        const response = await axios.get(url, options);
+        //can edit this function to return more data if needed
+        //calling apiById(with actor.id) will return all data for now
+        const actors = response.data.results.map((actor: any) => ({
+            id:actor.id,
+            name:actor.name,
+            image:actor.profile_path,
+        }));
+        return actors;
+    }
+    catch(error){
+        console.error('Error searching actors', error);
+        throw error;
+    }
+}
+
+//searchTvShows given a searchString returns a list of tv shows and 
+//their information filtered by a map
+const searchTvShows = async (searchString: string) => {
+    const url = 'https://api.themoviedb.org/3/search/tv?api_key='+apiKey+'&query='+searchString;
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+apiAccessToken
+        }
+    };
+    try{
+        const response = await axios.get(url, options);
+        //can edit this function to return more data if needed
+        //calling apiById(with tvShow.id) will return all data for now
+        const tvShows = response.data.results.map((tvShow: any) => ({
+            id:tvShow.id,
+            name:tvShow.name,
+            image:tvShow.poster_path,
+            summary:tvShow.overview,
+            startdate:tvShow.first_air_date,
+        }));
+        return tvShows;
+    }
+    catch(error){
+        console.error('Error searching tv shows', error);
+        throw error;
+    }
+}
+
+//returns all data for a movie by id
+//no need for a route, will be behind the scenes
+const movieById = async (id: string) => {
+    const url = 'https://api.themoviedb.org/3/movie/'+id+'?api_key='+apiKey;
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+apiAccessToken
+        }
+    };
+    try{
+        const response = await axios.get(url, options);
+        //just returns all data for now, decide what to do with it later
+        return response.data;
+    }
+    catch(error){
+        console.error('Error searching for movie details', error);
+        throw error;
+    }
+}
+//personById returns all data for a person by id
+const personById = async (id: string) => {
+    const url = 'https://api.themoviedb.org/3/person/'+id+'?api_key='+apiKey;
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+apiAccessToken
+        }
+    };
+    try{
+        const response = await axios.get(url, options);
+        //just returns all data for now, decide what to do with it later
+        return response.data;
+    }
+    catch(error){
+        console.error('Error searching for person details', error);
+        throw error;
+    }
+}
+
+//nowPlaying returns a list of movies currently playing in theaters
+const nowPlaying = async () => {
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key='+apiKey;
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+apiAccessToken
+        }
+    };
+    try{
+        const response = await axios.get(url, options);
+        //can edit this function to return different data if needed
+        const movies = response.data.results.map((movie: any) => ({
+            id:movie.id,
+            title:movie.title,
+            image:movie.poster_path,
+            summary:movie.overview,
+        }));
+        return movies;
+    }
+    catch(error){
+        console.error('Error searching movies', error);
+        throw error;
+    }
+}
+
+//popularMovies returns a list of popular movies from TMDB
+const popularMovies = async () => {
+    const url = 'https://api.themoviedb.org/3/movie/popular?api_key='+apiKey;
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+apiAccessToken
+        }
+    };
+    try{
+        const response = await axios.get(url, options);
+        //can edit this function to return different data if needed
+        const movies = response.data.results.map((movie: any) => ({
+            id:movie.id,
+            title:movie.title,
+            image:movie.poster_path,
+            summary:movie.overview,
+        }));
+        return movies;
+    }
+    catch(error){
+        console.error('Error searching movies', error);
+        throw error;
+    }
+}
+
+
+
+
+
+export {searchMovies, movieById,searchByActor,personById,searchTvShows,nowPlaying,popularMovies};
+
 
 
 
--- back-end/routes/index.ts
+++ back-end/routes/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, Router } from "express";
-import { searchMovies } from "../apiPuller";
+import { searchByActor,movieById,personById,searchMovies,searchTvShows,nowPlaying,popularMovies } from "../apiPuller";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
@@ -10,6 +10,7 @@ router.get("/", (req: Request, res: Response) => {
 router.get("/search-movies", async (req: Request, res: Response) => {
   try{
     //may need to alter this to get the search string from the body properly
+    //searchString is the current key for the search
     const searchString = req.query.searchString as string;
     const movies = await searchMovies(searchString);
     res.json(movies);
@@ -20,4 +21,93 @@ router.get("/search-movies", async (req: Request, res: Response) => {
   }
 });
 
+//route to get movie details by id,calls movieById from apiPuller
+//result is all data right now, edit movieById to map results based on whats needed
+router.get("/details-movie", async (req: Request, res: Response) => {
+  try{
+    const id = req.query.id as string;
+    const movie = await movieById(id);
+    res.json(movie);
+  }
+  catch(error){
+    console.error('Error getting movie details', error);
+    res.status(500).send('Error getting movie details');
+  }
+});
+
+//route to search for actors calls searchByActor from apiPuller
+router.get("/search-actors", async (req: Request, res: Response) => {
+  try{
+    //may need to alter this to get the search string from the body properly
+    //searchString is the current key for the search
+    const searchString = req.query.searchString as string;
+    const actors = await searchByActor(searchString);
+    res.json(actors);
+  }
+  catch(error){
+    console.error('Error searching actors', error);
+    res.status(500).send('Error searching actors');
+  }
+});
+
+//route to get actor details by id, calls personById() from apiPuller
+router.get("/details-actor", async (req: Request, res: Response) => {
+  try{
+    const id = req.query.id as string;
+    const actor = await personById(id);
+    res.json(actor);
+  }
+  catch(error){
+    console.error('Error getting actor details', error);
+    res.status(500).send('Error getting actor details');
+  }
+});
+
+//route to search for tv shows, calls searchTvShows from apiPuller
+router.get("/search-tv", async (req: Request, res: Response) => {
+  try{
+    //searchString is the current key for the search
+    const searchString = req.query.searchString as string;
+    const tvShows = await searchTvShows(searchString);
+    res.json(tvShows);
+  }
+  catch(error){
+    console.error('Error searching tv shows', error);
+    res.status(500).send('Error searching tv shows');
+  }
+});
+
+
+
+
+/*Below are routes tor return movie lists(popular, now playing, top rated, by genre perhaps,
+  have the routes here for testing for now, likely wont need these until a home page is decided on
+*/
+
+//route to get now playing movies, calls nowPlaying from apiPuller
+router.get("/now-playing", async (req: Request, res: Response) => {
+  try{
+    const movies = await nowPlaying();
+    res.json(movies);
+  }
+  catch(error){
+    console.error('Error getting now playing movies', error);
+    res.status(500).send('Error getting now playing movies');
+  }
+});
+
+//route to get popular movies calls popularMovies from apiPuller
+router.get("/popular-movies", async (req: Request, res: Response) => {
+  try{
+    const movies = await popularMovies();
+    res.json(movies);
+  }
+  catch(error){
+    console.error('Error getting popular movies', error);
+    res.status(500).send('Error getting popular movies');
+  }
+});
+
+
+//search
 export default router;
