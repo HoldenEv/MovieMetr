d1ec4346d38840b0be2e9a854ce1f91896931d99
Tobe Ohajunwa <alexohajunwa@gmail.com>
2024-02-27 11:43:37 -0800

    looks fine on phones but not larger displays

--- frontend/app/userpage/page.tsx
+++ frontend/app/userpage/page.tsx
@@ -3,36 +3,49 @@ import "./userpage.css";
 export default function Userpage() {
   return (
     <div className="userpage">
-        <div className="userinfo">
-            <div>
-                <img 
-                    src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png" 
-                    alt="Profile Picture" 
-                    className="profile-picture" 
-                />
-            </div>
-            <div className="username">
-                <p className="username-text">FxrtSniffer690</p>
-                <div className="overview">
-                    <p>300 films</p>
-                    <p>300 followers</p>
-                    <p>300 following</p>
-                </div>
-            </div>
+      <div className="userinfo">
+        <div>
+          <img
+            src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png"
+            alt="Profile Picture"
+            className="profile-picture"
+          />
         </div>
-        <div className="gallery">
-            <img src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png" className="gallery-item"/>
-            <img src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png" className="gallery-item"/>
-            <img src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png" className="gallery-item"/>
-            <img src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png" className="gallery-item"/>
-            <img src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png" className="gallery-item"/>
-            <img src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png" className="gallery-item"/>
+        <div className="username">
+          <p className="username-text">FxrtSniffer690</p>
+          <div className="overview">
+            <p>300 films</p>
+            <p>300 followers</p>
+            <p>300 following</p>
+          </div>
         </div>
+      </div>
+      <div className="gallery">
+        <img
+          src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png"
+          className="gallery-item"
+        />
+        <img
+          src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png"
+          className="gallery-item"
+        />
+        <img
+          src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png"
+          className="gallery-item"
+        />
+        <img
+          src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png"
+          className="gallery-item"
+        />
+        <img
+          src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png"
+          className="gallery-item"
+        />
+        <img
+          src="https://chicagomaroon.com/wp-content/uploads/2021/10/squid-game-900x531.png"
+          className="gallery-item"
+        />
+      </div>
     </div>
-
-
-
-
-  )
+  );
 }
-
