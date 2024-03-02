a9be427a171bcbca6d2cbce15a46f22d6c4e6b73
Lukas Chou <lukaschou21@icloud.com>
2024-03-01 16:08:51 -0800

    styles for login

--- back-end/routes/authenticationRoutes.ts
+++ back-end/routes/authenticationRoutes.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from "express";
-import User from "../models/user"; 
-import accountController from '../controllers/accountController';
-const authenticationMiddleware = require('../middleware/authentication');
-const LocalStrategy = require('passport-local');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const mongoose = require('mongoose');
+import User from "../models/user";
+import accountController from "../controllers/accountController";
+const authenticationMiddleware = require("../middleware/authentication");
+const LocalStrategy = require("passport-local");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const mongoose = require("mongoose");
 import * as dotenv from "dotenv";
 const router = Router();
 dotenv.config();
@@ -14,18 +14,27 @@ router.use(bodyParser.urlencoded({ extended: false }));
 mongoose.connect(process.env.URI);
 router.use(passport.initialize());
 
-passport.use(new LocalStrategy({
-  usernameField: 'username',  
-  passwordField: 'password',  
-  session: false 
-}, User.authenticate()));
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+      session: false,
+    },
+    User.authenticate()
+  )
+);
 passport.serializeUser(User.serializeUser());
 
-router.get('/', (req : Request, res : Response) => { res.send('Nothin Here') });
-router.get('/profile', passport.authenticate('jwt', { session: false }), accountController.profile);
-router.post('/login', accountController.login);
-router.post('/register', accountController.register);
-
-
+router.get("/", (req: Request, res: Response) => {
+  res.send("Nothin Here");
+});
+router.get(
+  "/profile",
+  passport.authenticate("jwt", { session: false }),
+  accountController.profile
+);
+router.post("/login", accountController.login);
+router.post("/register", accountController.register);
 
 export default router;
