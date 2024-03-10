7ab3baf5f4fc5705eb8860d020378ab460bb5aa6
Lukas Chou <lukaschou21@icloud.com>
2024-03-10 16:49:54 -0700

    refactored validation logic:

--- back-end/routes/authenticationRoutes.ts
+++ back-end/routes/authenticationRoutes.ts
@@ -1,7 +1,10 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import {
+  validateRegisterInput,
+  validate,
+} from "../middleware/validators/authenticationValidators";
 import User from "../models/user";
 import accountController from "../controllers/accountController";
-import { body, validationResult } from "express-validator";
 
 const authenticationMiddleware = require("../middleware/authentication");
 const LocalStrategy = require("passport-local");
@@ -15,32 +18,6 @@ dotenv.config();
 router.use(bodyParser.urlencoded({ extended: false }));
 mongoose.connect(process.env.URI);
 router.use(passport.initialize());
-
-const validateRegisterInput = [
-  body("username").notEmpty(),
-  body("email").isEmail(),
-  body("password")
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long")
-    .matches(/[A-Z]/)
-    .withMessage("Password must contain at least one uppercase letter")
-    .matches(/[0-9]/)
-    .withMessage("Password must contain at least one number"),
-  body("confirmPassword").custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("Passwords do not match");
-    }
-    return true;
-  }),
-];
-
-const validate = (req: Request, res: Response, next: Function) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  next();
-};
 
 passport.use(
   new LocalStrategy(
