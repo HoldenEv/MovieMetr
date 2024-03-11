552a1c2242ab29d9c259e2f5e8af96039f35ad7d
Nick Li <nmli@calpoly.edu>
2024-03-10 18:51:51 -0700

    couple edits to user stuff

--- back-end/controllers/accountController.ts
+++ back-end/controllers/accountController.ts
@@ -1,107 +1,9 @@
-
-// import { Request, Response } from "express";
-// import  User  from "../models/user";
-// import config from "../config";
-// import jwt from "jwt-simple";
-
-// // ideally we should refactor this code to use async/await, 
-// //and not handle any of the http response codes or req/res in the controller
-// //and instead handle that in the route, that way we can test the controller
-// //and call the functions from other controllers
-// const login = function (req: Request, res: Response) {
-//   //needs to check salt and hash of password to verify user
-//   User.findOne({ username: req.body.username }).exec()  
-//     .then((user: any) => {
-//       if (!user) {
-//         return res.status(404).json({ message: "User not found" });
-//       }
-
-//       var payload = {
-//         id: user.id,
-//         expire: Date.now() + 1000 * 60 * 60 * 24 * 7,
-//       };
-
-//       var token;
-//       if (config.jwtSecret) { // if it exists 
-//         // Pass { session: false } as an option to indicate that sessions are not needed
-//         token = jwt.encode(payload, config.jwtSecret);
-//       }
-
-//       res.json({ token: token });
-//     }).catch((err: Error) => {
-//       console.error("Error:", err);
-//       res.status(500).json({ message: "Internal Server Error" });
-//     });
-// };
-
-// const register = function (req : Request, res : Response) {
-//   User.register(
-//     new User({ 
-//       email: req.body.email,
-//       username: req.body.username 
-//     }), req.body.password, function (err : Error, msg : String) {
-//       if (err) {
-//         res.send(err);
-//       } else {
-//         res.send({ message: "Successful" });
-//       }
-//     }
-//   );
-// };
-
-// const profile = function(req : Request, res : Response) {
-//   res.json({
-//     message: 'You made it to the secured profile',
-//     user: req.user,
-//     token: req.query.secret_token
-//   })
-// }
-
-// const update = function(req : Request, res : Response) {
-//    const userId = req.body.userId;
-
-//    // Check if the ID is valid
-//    if (!userId) {
-//      return res.status(400).json({ message: "Invalid user ID" });
-//    }
-
-//    const updateFields = {
-//      email: req.body.email,
-//      username: req.body.username,
-//    };
-   
-//    // Find user by userId
-//    User.findOneAndUpdate({ _id: userId }, updateFields, { new: true })
-//      .then((updatedUser) => {
-//        if (!updatedUser) {
-//          return res.status(404).json({ message: "User not found" });
-//        }
- 
-//        res.json({ message: "User updated successfully", user: updatedUser });
-//      })
-//      .catch((err) => {
-//        console.error("Error:", err);
-//        res.status(500).json({ message: "Internal Server Error" });
-//      });
-// }
-
-// export default {
-//   login,
-//   register,
-//   profile,
-//   update
-// };
-
-
-
 import  User  from "../models/user";
 import config from "../config";
 import jwt from "jwt-simple";
 
-// ideally we should refactor this code to use async/await, 
-// and not handle any of the http response codes or req/res in the controller
-// and instead handle that in the route, that way we can test the controller
-// and call the functions from other controllers
+//responsible for logging in a user, takes in a username and returns a token
+//password verificationis not yet handled
 const loginUser = async (username: string) => {
       const user = await User.findOne({ username: username }).exec();
         if(!user) {
@@ -123,6 +25,8 @@ const loginUser = async (username: string) => {
       return { token: token };
 };
 
+/*registers a user, takes in an email, username, and password, 
+calls passport-local-mongoose's register function*/
 const registerUser = async(email: string, username: string, password: string) => {
   const user= new User({ 
     email: email,
