af3a8bb7f3f00de13375b939c87b3d3c4fa19128
Holden <holdenevers@gmail.com>
2024-05-29 13:37:00 -0700

    added mongoose to front ed

---
+++ frontend/database/awsObjectUrlSchema.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+export type awsImageUrl = {
+  objectUrl: string;
+};
+
+const awsObjectUrlSchema = new Schema<awsImageUrl>({
+  objectUrl: { type: String, required: true, unique: false },
+});
+
+const awsObjectUrl =
+  mongoose.models["awsObjectUrl"] ||
+  mongoose.model("awsObjectUrl", awsObjectUrlSchema);
+
+export default awsObjectUrl;
