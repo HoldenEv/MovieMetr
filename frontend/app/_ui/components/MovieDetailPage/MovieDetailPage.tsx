5e851cebd9a4dd6201a70cf40cd8def3be353aee
Lukas Chou <lukaschou21@icloud.com>
2024-05-31 00:05:48 -0700

    movie detail crash fix, change link on search result

--- frontend/app/_ui/components/MovieDetailPage/MovieDetailPage.tsx
+++ frontend/app/_ui/components/MovieDetailPage/MovieDetailPage.tsx
@@ -49,9 +49,9 @@ export default function MovieDetailPage({
             className={styles.posterimage}
             priority
             src={
-              data.poster_path 
-              ? `https://image.tmdb.org/t/p/original${data.poster_path}`
-              : notfound
+              data.poster_path
+                ? `https://image.tmdb.org/t/p/original${data.poster_path}`
+                : notfound
             }
             width={255}
             height={382.5}
@@ -69,52 +69,56 @@ export default function MovieDetailPage({
               <label> Running time</label>
               <span> {data.runtime} minutes</span>
             </div>
-            <div className={styles.set}>
-              <label> Genre</label>
-              <span>
-                {data.genres.map((genre: any, index: number) => (
-                  <span key={genre.id}>
-                    {genre.name}
-                    {index !== data.genres.length - 1 && "/"}{" "}
-                    {/* add '/' if it's not the last genre */}
-                  </span>
-                ))}
-              </span>
-            </div>
+            {data.genres && (
+              <div className={styles.set}>
+                <label> Genre</label>
+                <span>
+                  {data.genres.map((genre: any, index: number) => (
+                    <span key={genre.id}>
+                      {genre.name}
+                      {index !== data.genres.length - 1 && "/"}{" "}
+                      {/* add '/' if it's not the last genre */}
+                    </span>
+                  ))}
+                </span>
+              </div>
+            )}
             <div className={styles.set}>
               <label> About </label>
               <span>{data.tagline}</span>
               <div className={styles.moviedescription}>{data.overview}</div>
             </div>
-            <div className={styles.set}>
-              <label> Cast </label>
-              <div className={styles.horizontalScroll}>
-                {data.credits.cast.map((entry: any, id: any) => (
-                  <div key={id} className={styles.imageItemContainer}>
-                    <div className={styles.imageItem}>
-                      {entry.profile_path ? (
-                        <Image
-                          className={styles.castImage}
-                          src={`https://image.tmdb.org/t/p/original${entry.profile_path}`}
-                          alt={entry.id}
-                          width={50}
-                          height={70}
-                          style={{
-                            width: "100%",
-                            height: "100%",
-                            objectFit: "cover",
-                          }}
-                        />
-                      ) : (
-                        entry.id
-                      )}
+            {data.credits && (
+              <div className={styles.set}>
+                <label> Cast </label>
+                <div className={styles.horizontalScroll}>
+                  {data.credits.cast.map((entry: any, id: any) => (
+                    <div key={id} className={styles.imageItemContainer}>
+                      <div className={styles.imageItem}>
+                        {entry.profile_path ? (
+                          <Image
+                            className={styles.castImage}
+                            src={`https://image.tmdb.org/t/p/original${entry.profile_path}`}
+                            alt={entry.id}
+                            width={50}
+                            height={70}
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              objectFit: "cover",
+                            }}
+                          />
+                        ) : (
+                          entry.id
+                        )}
+                      </div>
+                      <div className={styles.name}>{entry.original_name}</div>
+                      <div className={styles.character}> {entry.character}</div>
                     </div>
-                    <div className={styles.name}>{entry.original_name}</div>
-                    <div className={styles.character}> {entry.character}</div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
--- frontend/app/_ui/components/SearchResult/SearchResult.tsx
+++ frontend/app/_ui/components/SearchResult/SearchResult.tsx
@@ -27,66 +27,66 @@ export default function SearchResult({
   const linkSource = `/${type}/${data.id}`;
 
   return (
-    <div className={styles.searchResultContainer}>
-      <Link href={linkSource}>
-        <Image
-          // use our own "not found" image if source is not found from TMDB
-          src={
-            data.image
-              ? `https://image.tmdb.org/t/p/original${data.image}`
-              : notfound
-          }
-          width={100.8}
-          height={144}
-          alt={`Poster for ${data.title}`}
-          className={styles.moviePoster}
-        ></Image>
-      </Link>
-      <div className={styles.content}>
-        <div>
-          <div className={styles.titleContainer}>
-            <Link href={linkSource}>
-              <h2 className={styles.title}>
-                {type === "films" ? data.title : data.name}
-              </h2>
-            </Link>
-            <h3 className={styles.alternateTitle}>
-              {(type === "films" && data.original_title !== data.title) ||
-              (type === "shows" && data.original_name !== data.name)
-                ? ` \u2022 [${type === "films" ? data.original_title : data.original_name}]`
-                : ""}
-            </h3>
-          </div>
-          {type === "films" || type === "shows" ? (
-            <h3 className={styles.year}>
-              ({type === "films" ? data.year : data.startdate})
-            </h3>
-          ) : (
-            <h3 className={styles.year}>
-              Known for{" "}
-              <span className={styles.italic}>{data.known_for_department}</span>
-            </h3>
-          )}
-        </div>
-        <div>
-          {type === "people" && data.known_for.length > 0 ? (
-            <div className={styles.knownForContainer}>
-              <h4 className={styles.notableWorksLabel}>Notable works: </h4>
-              {data.known_for.map((work: any, index: number) => {
-                return (
-                  <span key={work.id}>
-                    <span className={styles.knownForWork}>{work.title}</span>
-                    {index < data.known_for.length - 1 ? "," : ""}{" "}
-                  </span>
-                );
-              })}
+    <Link href={linkSource}>
+      <div className={styles.searchResultContainer}>
+          <Image
+            // use our own "not found" image if source is not found from TMDB
+            src={
+              data.image
+                ? `https://image.tmdb.org/t/p/original${data.image}`
+                : notfound
+            }
+            width={100.8}
+            height={144}
+            alt={`Poster for ${data.title}`}
+            className={styles.moviePoster}
+          ></Image>
+        <div className={styles.content}>
+          <div>
+            <div className={styles.titleContainer}>
+                <h2 className={styles.title}>
+                  {type === "films" ? data.title : data.name}
+                </h2>
+              <h3 className={styles.alternateTitle}>
+                {(type === "films" && data.original_title !== data.title) ||
+                (type === "shows" && data.original_name !== data.name)
+                  ? ` \u2022 [${type === "films" ? data.original_title : data.original_name}]`
+                  : ""}
+              </h3>
             </div>
-          ) : (
-            ""
-          )}
-          <p className={styles.summary}>{summary}</p>
+            {type === "films" || type === "shows" ? (
+              <h3 className={styles.year}>
+                ({type === "films" ? data.year : data.startdate})
+              </h3>
+            ) : (
+              <h3 className={styles.year}>
+                Known for{" "}
+                <span className={styles.italic}>
+                  {data.known_for_department}
+                </span>
+              </h3>
+            )}
+          </div>
+          <div>
+            {type === "people" && data.known_for.length > 0 ? (
+              <div className={styles.knownForContainer}>
+                <h4 className={styles.notableWorksLabel}>Notable works: </h4>
+                {data.known_for.map((work: any, index: number) => {
+                  return (
+                    <span key={work.id}>
+                      <span className={styles.knownForWork}>{work.title}</span>
+                      {index < data.known_for.length - 1 ? "," : ""}{" "}
+                    </span>
+                  );
+                })}
+              </div>
+            ) : (
+              ""
+            )}
+            <p className={styles.summary}>{summary}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
