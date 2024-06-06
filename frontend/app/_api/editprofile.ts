a20b00693abd97a689b7fbf5f1e7865be6dd778b
Nick Li <nmli@calpoly.edu>
2024-06-06 02:03:17 -0700

    frontend profile pic update done

--- frontend/app/_api/editprofile.ts
+++ frontend/app/_api/editprofile.ts
@@ -1,6 +1,11 @@
 const BASE_URL: string =
   process.env.NEXT_PUBLIC_BACKEND_URL + "/authentication/";
 
+
+  interface UploadProfilePictureResponse {
+    imageUrl: string;
+  }
+
 export const getUser = async (userId: string): Promise<any> => {
   try {
     const response = await fetch(`${BASE_URL}/getUser?userId=${userId}`, {
@@ -46,24 +51,51 @@ export const updateUser = async (
 // New function to handle profile picture upload
 export const uploadProfilePicture = async (
   file: File,
-): Promise<string> => {
+): Promise<UploadProfilePictureResponse> => {
   const formData = new FormData();
   formData.append('file', file);
 
   try {
+    console.log('starting file upload in uploadProfilePicture in editprofile.ts');
     const response = await fetch(`${BASE_URL}/uploadProfilePicture`, {
       method: 'POST',
       body: formData,
     });
 
+    console.log('response from uploadProfilePicture in editprofile.ts:', response);
     if (!response.ok) {
       throw new Error('Error uploading profile picture');
     }
 
-    const data = await response.json();
-    return data.imageUrl;
+    const data : UploadProfilePictureResponse = await response.json();
+    console.log('data from uploadProfilePicture in editprofile.ts:', data);
+    return data;
   } catch (error: any) {
     console.error('Error uploading profile picture:', error.message);
+    throw error;
+  }
+};
+
+export const updateProfilePath = async (
+  userId: string,
+  profilePath: string,
+): Promise<any> => {
+  try {
+    const response = await fetch(`${BASE_URL}/updateProfilePath`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId, profilePath }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Internal server error");
+    }
+
+    return await response.json();
+  } catch (error: any) {
+    console.error("Error updating profile picture:", error.message);
     throw error;
   }
 };
